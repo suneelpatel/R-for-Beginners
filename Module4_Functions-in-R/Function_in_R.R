b12a488d0b6899a4f84f5bc4ffe2065c91c21a8b
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-09 19:40:09 +0530

    Add files via upload

---
+++ Module4_Functions-in-R/Function_in_R.R
@@ -0,0 +1,26 @@
+# ====================== Funtion R ============================
+
+# Built-in-Function
+
+# Create a sequence of numbers from 32 to 44.
+print(seq(32,44))
+
+# Find mean of numbers from 25 to 82.
+print(mean(25:82))
+
+# Find sum of numbers frm 41 to 68.
+print(sum(41:68))
+
+
+# User-defined Function
+
+# Create a function to print squares of numbers in sequence.
+new.function <- function(a) {
+  for(i in 1:a) {
+    b <- i^2
+    print(b)
+  }
+}
+
+# Call the function new.function supplying 6 as an argument.
+new.function(6)

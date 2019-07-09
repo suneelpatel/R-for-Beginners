e489d7b0ed68f1bf22eb0f28fc25a51c9ec965f8
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-09 17:56:59 +0530

    Add files via upload

---
+++ Module3_Conditional-Statement-in-R/Conditional Statement.R
@@ -0,0 +1,68 @@
+#----------------------------------Conditional Statement ----------------------------------
+
+#----------------If Statement-----------------------------------------------------------------
+
+num1=10
+num2=20
+
+if(num1<=num2){
+  print("Num1 is less or equal to Num2")
+  
+
+x<=2
+repeat {
+  x<= x^2
+  print(x)
+
+if(x>100) {
+  break    
+  }
+
+  
+#-------------- If Else IF Statement ----------------------------------------------------------
+
+#Example1:
+  
+if (2==1) {
+  print("1")
+} else if (2==2) {
+  print("2")
+} else {
+  print("3")
+}
+
+  
+#Example2: 
+
+Num1 = 5
+Num2 = 20
+
+if (Num1<Num2) { 
+  print("Num1 is lesser than Num2") 
+} else if( Num1 > Num2){
+  print( "Num2 is lesser than Num1") 
+} else {
+  print("Num1 and Num2 are Equal") }
+
+
+#Example3:
+
+x<-5
+if(x>5) {
+  print("x is greater than 5")
+} else if(x==5) {
+  print("x is equal to 5")
+} else {
+  print("x is not greater than 5")
+}
+
+
+# ---------------------IF Else -----------------------------------------
+
+#Example:
+x <- -5
+if(x > 0){
+  print("Non-negative number")
+} else {
+  print("Negative number")
+}
